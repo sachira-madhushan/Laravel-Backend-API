1a0b986d9008f1ea4fdc37ed5f5166f134cdbf30
who am i <62942231+sachira-madhushan@users.noreply.github.com>
2024-06-09 19:49:20 +0530

    JWT Testing

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware('auth:api');
 
 //post route
 
@@ -21,3 +22,18 @@ Route::put('/post/{id}',[PostController::class,'edit']);
 Route::delete('/post/{id}',[PostController::class,'destroy']);
 
 //end post route
+
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', [AuthController::class,'login']);
+    Route::post('logout', [AuthController::class,'logout']);
+    Route::post('refresh', [AuthController::class,'refresh']);
+    Route::post('me', [AuthController::class,'me']);
+
+});
